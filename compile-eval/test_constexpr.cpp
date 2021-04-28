89c7c7504bca8b7bbed3e764b1d48f417ed78581
Andrey <crashc@yandex.ru>
2021-04-28 20:59:37 +0500

    Update test_constexpr.cpp
    Added checks for new task "Factorial".

--- compile-eval/test_constexpr.cpp
+++ compile-eval/test_constexpr.cpp
@@ -1,14 +1,32 @@
 #include "next_prime.h"
 #include "determinant.h"
+#include "factorial.h"
 
 void test_next_prime() {
-//     static_assert(next_prime(1) == 2, "fail");
-//     static_assert(next_prime(2) == 2, "fail");
-//     static_assert(next_prime(9) == 11, "fail");
-//     static_assert(next_prime(17230) == 17231, "fail");
-//     static_assert(next_prime(121) == 127, "fail");
-//     static_assert(next_prime(1000000) == 1000003, "fail");
-//     static_assert(next_prime(1000000000) == 1000000007, "fail");
+    static_assert(next_prime(1) == 2, "fail");
+    static_assert(next_prime(2) == 2, "fail");
+    static_assert(next_prime(9) == 11, "fail");
+    static_assert(next_prime(17230) == 17231, "fail");
+    static_assert(next_prime(121) == 127, "fail");
+    static_assert(next_prime(1000000) == 1000003, "fail");
+    static_assert(next_prime(1000000000) == 1000000007, "fail");
+}
+
+void test_factorial() {
+    static_assert(Factorial<0>::value == 1, "fail");
+    static_assert(Factorial<1>::value == 1, "fail");
+    static_assert(Factorial<2>::value == 2, "fail");
+    static_assert(Factorial<3>::value == 6, "fail");
+    static_assert(Factorial<4>::value == 24, "fail");
+    static_assert(Factorial<5>::value == 120, "fail");
+    static_assert(Factorial<6>::value == 720, "fail");
+    static_assert(Factorial<7>::value == 5040, "fail");
+    static_assert(Factorial<8>::value == 40320, "fail");
+    static_assert(Factorial<9>::value == 362880, "fail");
+    static_assert(Factorial<10>::value == 3628800, "fail");
+    static_assert(Factorial<11>::value == 39916800, "fail");
+    static_assert(Factorial<12>::value == 479001600, "fail");
+    static_assert(Factorial<13>::value == 6227020800, "fail");
 }
 
 void test_determinant() {
@@ -28,4 +46,5 @@ int main ()
 {
     test_next_prime();
     test_determinant();
+    test_factorial();
 }
